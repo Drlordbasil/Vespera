35662780b754ae837a98d8422e33c4bcb992ddab
Lord Basil <126736516+Drlordbasil@users.noreply.github.com>
2023-08-20 16:13:36 -0400

    Initial commit - AI_CoPilot.py

---
+++ AI_CoPilot.py
@@ -0,0 +1,47 @@
+Here is the AI chatbot that can assist the user in running the program locally on their PC:
+
+```python
+import subprocess
+
+class ChatBot:
+    def __init__(self):
+        self.commands = {
+            "run program": self.run_program,
+            "exit": self.exit_chatbot
+        }
+
+    def start_chat(self):
+        print("ChatBot: Hi! How can I assist you today? You can ask me to run the program by saying 'run program'.")
+        while True:
+            user_input = input("You: ")
+            self.process_input(user_input.lower())
+
+    def process_input(self, user_input):
+        if user_input in self.commands:
+            self.commands[user_input]()
+        else:
+            print("ChatBot: Sorry, I didn't understand that. Please try again.")
+
+    def run_program(self):
+        print("ChatBot: Running the program...")
+        result = subprocess.run(["python", "main.py"], capture_output=True, text=True)
+        if result.returncode == 0:
+            print("\nProgram executed successfully!")
+        else:
+            print(f"\nError executing program:\n{result.stderr}")
+
+    def exit_chatbot(self):
+        print("ChatBot: Goodbye!")
+        exit()
+
+if __name__ == "__main__":
+    chatbot = ChatBot()
+    chatbot.start_chat()
+```
+
+To run the chatbot, simply save the code as a Python file (e.g., chatbot.py) and execute it using the Python interpreter:
+```
+python chatbot.py
+```
+
+The chatbot will start and provide instructions on how to interact with it. When the user asks to run the program, the chatbot will execute the main.py script using the subprocess module. After execution, it will display whether the program executed successfully or encountered any errors. The user can also exit the chatbot by typing "exit".
